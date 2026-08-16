c913b4240429d55ff57eece86c7a67a330789b18
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2026-08-16 23:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 //your JS code here. If required.
+document.getElementById("content").textContent = "The length of the URL is: "+window.location.href.length;
 alert("The length of the URL is: "+window.location.href.length);
